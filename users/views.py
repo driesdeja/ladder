d4a71941691197f74b000680956cb3db869bd262
dries <driesdeja@gmail.com>
2020-01-24 09:38:09 +1300

    Added the functionality to update the users' profile and account details.
    Worked on the user profile page - added additional stats.

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegistrationForm
-from round.models import PlayerRanking, PlayersInLadderRound, Match
+from .forms import UserRegistrationForm, PlayerUpdateForm, UserUpdateForm
+from round.models import PlayerRanking, PlayersInLadderRound, Match, Ladder
 from round.utils import matches_player_played_in
+
 
 # Create your views here.
 def register(request):
@@ -24,15 +25,35 @@ def register(request):
 
 @login_required
 def profile(request):
-    player_rankings = PlayerRanking.objects.filter(player=request.user.profile.player)
+    if request.POST:
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = PlayerUpdateForm(request.POST, instance=request.user.profile.player)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = PlayerUpdateForm(instance=request.user.profile.player)
+    player_rankings = PlayerRanking.objects.filter(player=request.user.profile.player).order_by('-last_updated')
     competed_in_rounds = PlayersInLadderRound.objects.filter(player=request.user.profile.player)
+
     ladder_rounds_competed_in = []
     for competed_in_round in competed_in_rounds:
         ladder_rounds_competed_in.append(competed_in_round.ladder_round)
+    ladders_competed_in = []
+    for ladder_round in ladder_rounds_competed_in:
+        ladders_competed_in.append(Ladder.objects.get(id=ladder_round.ladder.id))
+    set_ladders = set(ladders_competed_in)
+    ladders_competed_in = list(set_ladders)
     matches_played_in = matches_player_played_in(request.user.profile.player)
     context = {
         'player_rankings': player_rankings,
+        'ladders_competed_in': ladders_competed_in,
         'ladder_rounds_competed_in': ladder_rounds_competed_in,
-        'matches': matches_played_in
+        'matches': matches_played_in,
+        'u_form': u_form,
+        'p_form': p_form
     }
     return render(request, 'users/profile.html', context)
