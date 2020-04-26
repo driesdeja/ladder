816cbdd1aaa3ab0f6b078237c9f17dba823236aa
Dries de Jager <driesdeja@gmail.com>
2020-04-26 14:07:46 +1200

    Added the ability to download the players list as a pdf.  This is done using ReportLab.  Currently this sits in the players/util but will be refactored out as I gain more experience.

--- players/urls.py
+++ players/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('administration/create/', views.create_player, name='create-player'),
     path('administration/reset-ranking/', views.reset_rankings, name='reset-rankings'),
     path('administration/import-players', views.import_players, name='import-players'),
-    path('administration/export-players', views.export_players, name="export-players"),
+    path('administration/export-players', views.export_players, name='export-players'),
+    path('administration/download-player-list', views.download_players, name='download-players'),
     path('', views.list_players, name='list-players'),
 
 ]
--- players/utils.py
+++ players/utils.py
@@ -1,10 +1,14 @@
 import csv
 import os
 import json
+import io
 from io import TextIOWrapper
 from .models import Player
 from django.conf import settings
-
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Table, Image, Spacer
+from reportlab.platypus.tables import TableStyle
 
 
 def get_file_of_players():
@@ -47,3 +51,56 @@ def save_players(players):
                               email=player['email'],
                               contact_number=player['contact_number'])
     return len(players_as_json)
+
+
+def get_pdf_file(data_to_download):
+    players = Player.objects.all()
+    player_list = []
+    for each_player in players:
+        player_list.append([each_player.ranking,
+                            each_player.first_name,
+                            each_player.last_name,
+                            each_player.email,
+                            each_player.contact_number])
+
+    doc = io.BytesIO()
+
+    pdf = SimpleDocTemplate(doc, pagesize=A4)
+
+    # Header Table with the logos
+    franklin_logo = os.path.join(settings.MEDIA_ROOT, 'images/franklin_logo.png')
+    squash_auckland_logo = os.path.join(settings.MEDIA_ROOT, 'images/squash_auckland.png')
+    franklin_logo_image = Image(franklin_logo)
+    franklin_logo_image.drawWidth = 100
+    franklin_logo_image.drawHeight = 55
+
+    squash_auckland_logo_image = Image(squash_auckland_logo)
+    squash_auckland_logo_image.drawWidth = 100
+    squash_auckland_logo_image.drawHeight = 30
+
+    document_title = "Franklin Squash Club Ladder"
+
+    header_table = Table([[franklin_logo_image, document_title, squash_auckland_logo_image]])
+
+    # Header Table Style
+    header_table_style = TableStyle([
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('TEXTCOLOR', (1, 0), (1, 0), colors.HexColor('#CD6620')),
+        ('FONTSIZE', (1, 0), (1, 0), 20),
+        ('FONTNAME', (1, 0), (1, 0), 'Times-BoldItalic')
+    ])
+
+    header_table.setStyle(header_table_style)
+
+    spacer = Spacer(1, 10)
+
+    # List with all of the players.
+    player_list_table = Table(player_list)
+
+    page_elements = [header_table, spacer, player_list_table]
+
+    pdf.build(page_elements)
+
+    doc.seek(0)
+
+    return doc
