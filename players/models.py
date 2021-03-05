1fb2459e39375a9aae07f62f600b6c71d8307c6f
Dries <driesdeja@gmail.com>
2021-03-06 08:04:44 +1300

    Added the status to the Player module.
    Added the PlayerStatus model to track the changes in player status
    
    added the .vscode to gitignore

--- players/models.py
+++ players/models.py
@@ -2,14 +2,33 @@ from django.db import models
 
 
 class Player(models.Model):
+    ACTIVE = 0
+    INACTIVE = 1
+    DISABLED = 2
+
+    Status = [
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+        (DISABLED, 'Disabled')
+    ]
+    
+    
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     contact_number = models.CharField(max_length=30)
     ranking = models.IntegerField(default=0)
     email = models.EmailField(blank=True)
-
+    status = models.IntegerField(choices=Status, default=ACTIVE)
     class Meta:
         ordering = ['last_name']
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class PlayerStatus(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    status = models.IntegerField()
+    from_date = models.DateTimeField()
+    to_date = models.DateTimeField(null=True)
+    updated_date = models.DateTimeField(auto_now=True)
